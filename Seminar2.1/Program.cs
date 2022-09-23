8d1e61db61447ed2df63bb8881ee6084183493f3
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-23 14:36:08 +0300

    показываем наибольшее число случайного двузначного числа из промежутка

---
+++ Seminar2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+int number = new Random().Next(10, 100);
+int num1 = number / 10;
+int num2 = number % 10;
+int max = num1;
+
+if (max<num2)
+{
+    max=num2;
+}
+Console.WriteLine($"Максимальная цифра числа {number} = {max}");
