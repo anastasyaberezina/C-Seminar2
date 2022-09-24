db40a79557243f33e94840d0f81dfd323a6ab11c
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-24 15:58:40 +0300

    кратное ли число 23 и 7

---
+++ Seminar2.4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает число и проверяет кратность на 7 и 23.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number%7 == 0)&&(number%23 == 0))
+{
+    Console.WriteLine($"Число {number} является кратным 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} НЕ является кратным 7 и 23");
+}
