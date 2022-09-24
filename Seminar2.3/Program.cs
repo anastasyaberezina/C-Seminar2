6fc373049587d9c8d95a4e09b184fa4b986612a2
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-24 15:29:32 +0300

    является ли второе число кратное первому

---
+++ Seminar2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Принимает 2 числа и выводит, является ли второе число кратным первому.
+
+Console.Write("Введите двузначное число: ");
+int number = int.Parse(Console.ReadLine());
+int number1 = number / 10;
+int number2 = number % 10;
+
+if (number1%number2==0)
+{
+    Console.WriteLine($"Число {number2} является кратным числу {number1}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} НЕ является кратным числу {number1}");
+}
+
